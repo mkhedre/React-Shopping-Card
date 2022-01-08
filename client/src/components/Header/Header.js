ff829da548f5effd2abc3c6718f00aa12af2b621
mkhedre <mkhedre3@gmail.com>
2022-01-08 11:06:16 +0200

    header-comp

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../../css/Header/Header.css'
+export default function Header() {
+    return (
+        <header>
+            shopping card
+      </header>
+    )
+}

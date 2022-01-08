36520b13b7de59316deaf93b3909c3e83837b38b
mkhedre <mkhedre3@gmail.com>
2022-01-09 00:57:05 +0200

    add products

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Product from "./components/products/product";
+import data from'./data.json'
 function App() {
+  const [product ,setproduct] = useState(data)
+  
+  const mapping=()=>{
+    product.map((item)=>{
+      return(
+        <div className="productitem">
+          <h2>{item.title}</h2>
+          <img src={item.imageurl} />
+          <p>{item.desc}</p>
+        </div>
+      )
+    })
+  }
   return (
+
     <div className="layout">
       <Header />
-      <main>main</main>
+      <main>
+        <div className="wrapper">
+          <Product products={product} />
+          <div className="filter">filter</div>
+        </div>
+      </main>
       <Footer />
     </div>
   );
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../../css/Header/Header.css'
-export default function Header() {
+function Header() {
     return (
         <header>
             shopping card
       </header>
     )
 }
+export default Header
